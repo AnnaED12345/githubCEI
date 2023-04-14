60dec1c4689a03b27ba4e6fabd77ffb072e098e5
Anna <aniemildimitrova@alumnos.cei.es>
2023-04-14 23:08:51 +0200

    Viernes 14 abril (entregables)

--- ejerciciosdesdecero.js
+++ ejerciciosdesdecero.js
@@ -4,7 +4,7 @@
 /*prompt= cuadro de dialogo 
 promt ('texto que quieras añadir')*/
 
-let nombre = prompt('¿Cómo te llamas');
+let nombre = prompt('¿Cómo te llamas?');
 /* para que aparezca en pantalla usamos console.log("entre comillas el
 texto que queramos que aparezca" + variable)
 console.log('Hola,' + nombre)*/
