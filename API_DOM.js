cc19757ac299aff29daebb8afdd8e9cb4e1f5fc0
Anna <aniemildimitrova@alumnos.cei.es>
2023-04-20 10:39:40 +0200

    jueves 20 abril

---
+++ API_DOM.js
@@ -0,0 +1,6 @@
+/*API DOM
+Ejercicio 1: Crea una página HTML con un párrafo que tenga un ID. 
+Escribe un script de JavaScript que utilice getElementById para seleccionar 
+el párrafo y cambiar su contenido a "¡Hola, mundo!". 
+*/
+
