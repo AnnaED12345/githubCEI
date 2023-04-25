b1cead35776d5271342136c35a5cb5af12bd4a59
Anna <aniemildimitrova@alumnos.cei.es>
2023-04-25 11:21:51 +0200

    25 abril

--- entregable_temporizador.js
+++ entregable_temporizador.js
@@ -1,16 +1,47 @@
 
 const btnIniciar = document.getElementById("boton_enviar");
+const contador = document.getElementById("contador");
 
-//función iniciar
-function iniciarTemporizador (tiempo) {
-    tiempo.preventDefault(); //para evitar que el comportamiento predeterminado de un evento ocurra.
+
+//prueba 1
+// //función para guardar los numeros ingresados 
+// let numerosIngresados = function (tiempo) {
+//     tiempo.preventDefault(); //para evitar que el comportamiento predeterminado de un evento ocurra.
+//     let minutos = document.getElementById("minutosID").value;
+//     let segundos = document.getElementById("segundosID").value;
+
+//     console.log(minutos, segundos);
+// }
+
+
+// Evento INICIAR
+
+// btnIniciar.addEventListener("click", numerosIngresados);
+
+
+
+//prueba 2
+//función para guardar los numeros ingresados
+let numerosIngresados = function (tiempo) {
     let minutos = document.getElementById("minutosID").value;
     let segundos = document.getElementById("segundosID").value;
-
-    console.log(minutos, segundos);
+    return(minutos * 60) + segundos;
 }
 
-//crear el evento INICIAR
+numerosIngresados();
 
-btnIniciar.addEventListener("click", iniciarTemporizador);
+function mostrarTiempo(tiempoRestante) {
+    var minutos = Math.floor(tiempoRestante / 60);
+    var segundos = tiempoRestante % 60;
+    var tiempoMostrado = document.getElementById("contador");
+    tiempoMostrado.innerHTML = minutos + ":" + segundos;
+  }
+
+
+// //Función contador
+// function temporizador () {
+    
+    
+// }
+
 
