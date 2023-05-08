4ee285be74509cd115fa6dc3d3df608ea2d0920a
[Anna] <[aniemildimitrova@alumnos.cei.es]>
2023-05-08 15:00:44 +0200

    08 Mayo

--- node.js/TODOs/todos.js
+++ node.js/TODOs/todos.js
@@ -8,20 +8,27 @@ Backend (ExpressJS):
 
 const express = require("express"); 
 const app = express();
-const tareas = [];
+const bodyParser = require("body-parser");
+const tareas = ["Estudiar", "Limpiar", "Comprar"];
 
 app.use(bodyParser.json()); //middleware que pasa el body a req.body 
 
-app.get("/tareas", (req, res) => {
-    res.send(tareas);
+app.get("/", (req, res) => { //con el metodo get leemos los recursos
+    res.send(tareas); //respondemos enviando el array de tareas
 });
 
-app.post("/tareas", (req, res) => {
-    //tareas es el array y push actualiza el array
-    tareas.push(req.body.descripcion); 
+app.post("/", (req, res) => { //post para añadir tareas 
+    if (req.body.tarea === undefined){ //si no se añade ninguna tarea, da error
+        res.status(400).send("Tienes que añadir una tarea") 
+    }
+    else{ //si se añade 
+        tareas.push(req.body.tarea); //push actualiza el array
+        res.status(201).send("Se ha añadido correctamente");
+    };
 });
 
-app.put("/tareas/:id", (req, res) => { //id es para identificar la tarea --> id es la posicion en el array
+app.put("/tareas/:id", (req, res) => { //put para actualizar las tareas
+    //id es para identificar la tarea --> id es la posicion en el array
     res.status(201).send(tareas[id]);
 });
 
