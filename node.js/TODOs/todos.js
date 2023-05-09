5dcb647bce900c1ab50fa7776dd14b34a933c61a
Anna <aniemildimitrova@alumnos.cei.es>
2023-05-09 10:49:50 +0200

    9 mayo

--- node.js/TODOs/todos.js
+++ node.js/TODOs/todos.js
@@ -30,19 +30,27 @@ app.post("/", (req, res) => { //post para añadir tareas
 
 app.put("/tareas/:id", (req, res) => { //put para actualizar las tareas
     //id es para identificar una tarea concreta --> id = posicion en el array
-    //si nuestro id son números deberiamos pasarlo a string con paresInt
-    const tareasID = parseInt(req.params.id); //en este caso params por que estas accediendo al parametro y no al body
-    if (tareasID < 0) { //si el id es menor a 0 no existe
-        res.status(404).send("No existe esta tarea")
+    const tareasID = (req.params.id); //en este caso params por que estas accediendo al parametro y no al body
+    if (tareasID < 0 || tareasID > tareas.length) { //si el id es menor a 0 no existe - error
+        res.status(400).send("No existe esta tarea")
     }
     else { //aquí debemos actualizar la tarea
-            tareas[tareasID] = req.body.tarea;
-            res.status(201).send(`Se ha actualizado la tarea ${tareasID}`);
+        tareas[tareasID] = req.body.tarea; //buscamos en el array "tareas" la posición de la tarea "tareaID" y lo actualizamos en el body
+        res.status(201).send(`Se ha actualizado la tarea ${tareasID}`);
         }
 });
 
-app.delete("/", (req, res) => {
-    res.send("Esta funcionando método DELETE")
+app.delete("/tareas/:id", (req, res) => { //elimina tareas
+    //parseInt() --> convierte una cadena de caracteres a un número entero. 
+    //En este caso, se utiliza para convertir el valor del parámetro id que se obtiene de la URL a un número entero, que se utiliza para identificar la tarea que se desea eliminar del array.
+    const tareasID = parseInt(req.params.id); //identificamos tarea
+    if (tareasID < 0 || tareasID > tareas.length) { //si no la identifica - error
+        res.status(400).send("No existe esta tarea") 
+    } 
+    else {//destructuración de array - splice
+        tareas.splice(tareasID, 1); //sintaxis: slice(start, end) --> es necesario poner en cuál empieza y en cuál termina, para determinar cuántas quieres borrar.
+        res.send(`Se ha eliminado la tarea ${tareasID}`);
+    }
 });
 
 
