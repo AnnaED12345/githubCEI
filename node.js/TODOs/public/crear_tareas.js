e2cc6d5d39d345f7bd7b906bc58020fae3955c37
Anna <aniemildimitrova@alumnos.cei.es>
2023-05-11 11:30:07 +0200

    11 mayo

--- node.js/TODOs/public/crear_tareas.js
+++ node.js/TODOs/public/crear_tareas.js
@@ -7,43 +7,58 @@
 4.Añadir tarea
  */
 
-const btnAnadir = document.getElementById("btnAnadir");
+const btnAnadir = document.getElementById("btnAnadir"); 
 const arrayInputs = document.getElementsByClassName("texto");
+const listaTareas2 = document.getElementById("listaTareas"); //guardamos en una variable el elemento html dónde queremos meter las tareas
+const tarea = document.getElementById("tareaID");
 
-//funcion recoger tarea:
+//FUNCIÓN PARA RECOGER TAREA: 
 const recogerTarea = function (event) {
     event.preventDefault();
 
-    //El fetch debe de estar dentro de la función ya que queremos que cada vez que se recoja una tarea se haga una petición al servidor y que este la pinte en la lista de tareas.
-    
+    //options (debe estar antes del fetch ya que se usa en el fetch) -- metodo POST
     const body = {tarea: tarea} //en el body pasamos la estructura: pedimos un objeto que pide la propiedad tarea que recoge el valor de tarea tareaID.value
-    
-    //fetch al servidor - método post
+
     const options = {
         method: 'POST', //por defecto método GET --> cambiamos a POST
-        body: JSON.stringify(body),
+        body: JSON.stringify(body), //lo enviamos en formato json 
         headers: {
-            "Content-Type": "application/json" //
+            "Content-Type": "application/json" //informamos de que estamos enviando un formato json
         }
     }
 
+    // FETCH AL SERVIDOR
     const post = fetch("/tareas", options); //guardamos la petición en una variable 
-    console.log(post); //la imprimimos por pantalla para ver que recibimos de la petición
+
     
-    const listaTareas = document.getElementById("listaTareas"); //guardamos en una variable el elemento html dónde queremos meter las tareas
 
-    post.then ((responseDatos) => { //devuelve promesa
+    post.then((response) => { //devuelve promesa
+        post.then((res) => {
+            if (res.ok) { // comprobamos que la respuesta sea correcta
+              return response.text(); // si es correcta, devolvemos el texto de la respuesta
+            } else {
+             console.error('Error en la petición'); // si no es correcta, lanzamos un error
+            }
+        }) .then((tareaAgregada) => { 
+            /* console.log(tareaAgregada); */
+            //entonces, si la respuesta es correcta: 
+            const nuevaTarea = document.createElement('li');  // Creamos un nuevo elemento de lista para la tarea agregada
+            nuevaTarea.textContent = tareaAgregada; //asginamos a nueva tarea el texto
+
+            // Agregamos el nuevo elemento de lista al elemento de lista de tareas existente
+            listaTareas2.appendChild(nuevaTarea);
+        });  
+    });
+
+/*     post.then ((responseDatos) => { //devuelve promesa
         return responseDatos.json(); //responde como fichero json y lo pasamos a js
-    }) .then ((responseDatos2) => {
-        responseDatos2(tareaAgregada => { //función para guardar la tarea que recogemos: 
-            let tareaNueva = document.getElementById("tareaID").value; //guardamos en una variable el valor de tareaID
-            console.log(`Se ha añadido la tarea ${tarea}`);
+    }) .then ((tareaAgregada) => {
+        console.log(tareaAgregada);
+            let tareaNueva2 = document.createElement('li'); //creamos un li por cada tareaNueva
+            tareaNueva2.textContent = tareaAgregada; //se asigna el valor de los datos recogidos = tareaAgregada
+            listaTareas2.appendChild(tareaNueva2);//metemos tarea nueva en la lista de tareas (li dentro de ul) 
+        }) */
 
-            tareaNueva = document.createElement('li'); //creamos un li por cada tareaNueva
-            tareaNueva.textContent = tareaAgregada; //se asigna el valor de los datos recogidos = tareaAgregada
-            listaTareas.appendChild(tareaNueva); //metemos tarea nueva en la lista de tareas (li dentro de ul)
-    })
-})
 
 //listeners que inician la función 
 btnAnadir.addEventListener("click", recogerTarea);
