5b4a5245b3532bf3d364c4f417a1294b3ccfe5c2
[Anna] <[aniemildimitrova@alumnos.cei.es]>
2023-06-21 14:26:27 +0200

    21.06

---
+++ app_remix/server/test-usuarios.test.js
@@ -0,0 +1,10 @@
+const rutasUsuarios = require("./usuarios");
+
+/* rutasUsuarios(app) */
+
+test ("petición POST correcta a la URL específicada", () => {
+    app.post = jest.fn();
+    app.post();
+
+    expect(axios.get).toHaveBeenCalledWith("/users/:user_id/tasks");
+})
