0e2f796fdb5ecbfe954279ecacb73535ede72179
[Anna] <[aniemildimitrova@alumnos.cei.es]>
2023-06-21 14:50:15 +0200

    21.06

--- app_remix/server/test-usuarios.test.js
+++ app_remix/server/test-usuarios.test.js
@@ -6,5 +6,6 @@ test ("petición POST correcta a la URL específicada", () => {
     app.post = jest.fn();
     app.post();
 
-    expect(axios.get).toHaveBeenCalledWith("/users/:user_id/tasks");
+    expect(axios.get).toHaveBeenCalledWith("/users/:user_id/tasks", expect.anything());
 })
+
